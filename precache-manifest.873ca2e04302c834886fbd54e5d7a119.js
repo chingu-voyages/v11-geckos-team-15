b746c595db1e64a620d07ea426b4fc6d06d8fb5e
Noah Maizels <noahmaizels@hotmail.com>
2019-10-10 16:52:08 +0800

    Updates

--- precache-manifest.873ca2e04302c834886fbd54e5d7a119.js
+++ precache-manifest.873ca2e04302c834886fbd54e5d7a119.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "12fb31d91170dbbec9180c3c82e00574",
+    "revision": "7f9cd02cf14a14b7d2283d79f1accced",
     "url": "/v11-geckos-team-15/index.html"
   },
   {
-    "revision": "4874f9bd32b68a86221c",
-    "url": "/v11-geckos-team-15/static/css/main.b2c65157.chunk.css"
+    "revision": "011f3aba082e41568673",
+    "url": "/v11-geckos-team-15/static/css/main.0a6a790b.chunk.css"
   },
   {
     "revision": "8763e4e1ddda0fd9990a",
     "url": "/v11-geckos-team-15/static/js/2.5c921d7e.chunk.js"
   },
   {
-    "revision": "4874f9bd32b68a86221c",
-    "url": "/v11-geckos-team-15/static/js/main.f3e2f0cd.chunk.js"
+    "revision": "011f3aba082e41568673",
+    "url": "/v11-geckos-team-15/static/js/main.d970d0f4.chunk.js"
   },
   {
     "revision": "1652d05c0ab16fc764f9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/v11-geckos-team-15/precache-manifest.759f53019cb6eb4654337fd332bd6fa3.js"
+  "/v11-geckos-team-15/precache-manifest.873ca2e04302c834886fbd54e5d7a119.js"
 );
 
 self.addEventListener('message', (event) => {
