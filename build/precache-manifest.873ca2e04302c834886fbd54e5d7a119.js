f83addf9b84c17bf6d21135185b172b24c034759
Noah Maizels <noahmaizels@hotmail.com>
2019-10-10 17:03:10 +0800

    fix css

---
+++ build/precache-manifest.873ca2e04302c834886fbd54e5d7a119.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9cd02cf14a14b7d2283d79f1accced",
+    "url": "/v11-geckos-team-15/index.html"
+  },
+  {
+    "revision": "011f3aba082e41568673",
+    "url": "/v11-geckos-team-15/static/css/main.0a6a790b.chunk.css"
+  },
+  {
+    "revision": "8763e4e1ddda0fd9990a",
+    "url": "/v11-geckos-team-15/static/js/2.5c921d7e.chunk.js"
+  },
+  {
+    "revision": "011f3aba082e41568673",
+    "url": "/v11-geckos-team-15/static/js/main.d970d0f4.chunk.js"
+  },
+  {
+    "revision": "1652d05c0ab16fc764f9",
+    "url": "/v11-geckos-team-15/static/js/runtime~main.d14c8ce3.js"
+  }
+]);
