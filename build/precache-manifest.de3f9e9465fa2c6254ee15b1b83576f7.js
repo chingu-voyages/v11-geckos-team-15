335e29f482102ccce94293c5bbbd89ad3bad620d
Noah Maizels <noahmaizels@hotmail.com>
2019-10-11 17:24:31 +0800

    Fixed font size

--- build/precache-manifest.de3f9e9465fa2c6254ee15b1b83576f7.js
+++ build/precache-manifest.de3f9e9465fa2c6254ee15b1b83576f7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d28c254cdc21976119d8bcb2eba4d918",
+    "revision": "ab141e4653525f30c0810c0ed46cfcc0",
     "url": "/v11-geckos-team-15/index.html"
   },
   {
-    "revision": "d1759046948733022577",
-    "url": "/v11-geckos-team-15/static/css/main.398bcd28.chunk.css"
+    "revision": "a6bfe78a5e7d49208266",
+    "url": "/v11-geckos-team-15/static/css/main.644d695d.chunk.css"
   },
   {
     "revision": "779d442728702cb92d92",
     "url": "/v11-geckos-team-15/static/js/2.6e7cebf0.chunk.js"
   },
   {
-    "revision": "d1759046948733022577",
-    "url": "/v11-geckos-team-15/static/js/main.05a55c49.chunk.js"
+    "revision": "a6bfe78a5e7d49208266",
+    "url": "/v11-geckos-team-15/static/js/main.91bb3f90.chunk.js"
   },
   {
     "revision": "1652d05c0ab16fc764f9",
